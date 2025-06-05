f1b0c54ddf4ab89f76f8e5c1f86f40283fd62390
merlense <agustina.miranda@davinci.edu.ar>
2025-06-05 17:54:25 -0300

    Ventanas

--- Mineria/src/GUI/PantallaPrincipal.java
+++ Mineria/src/GUI/PantallaPrincipal.java
@@ -7,12 +7,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class PantallaPrincipal extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField txtAgustinaTrola;
+	private JTextField PaginaPrincipal;
 
 	/**
 	 * Launch the application.
@@ -40,12 +45,42 @@ public class PantallaPrincipal extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 		
-		txtAgustinaTrola = new JTextField();
-		txtAgustinaTrola.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		txtAgustinaTrola.setText("Mineria Minashi");
-		contentPane.add(txtAgustinaTrola);
-		txtAgustinaTrola.setColumns(10);
-	}
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(134, 12, 5, 22);
+		contentPane.add(textArea);
+		
+		PaginaPrincipal = new JTextField();
+		PaginaPrincipal.setBounds(134, 28, 156, 26);
+		PaginaPrincipal.setHorizontalAlignment(SwingConstants.CENTER);
+		PaginaPrincipal.setFont(new Font("Tahoma", Font.BOLD, 16));
+		PaginaPrincipal.setText("Mineria Minashi");
+		contentPane.add(PaginaPrincipal);
+		PaginaPrincipal.setColumns(10);
+		JButton btnComprar = new JButton("comprar");
+		btnComprar.setBounds(34, 152, 89, 23);
+		contentPane.add(btnComprar);
+		int cliente = (int)(Math.random()*20);
+		JOptionPane.showMessageDialog(null, cliente==0?"Es admin":"Es cliente");
+		if (cliente==0) {
+			//admin
+			btnComprar.setVisible(false);
+			
+			HomeAdmin admin = new HomeAdmin();
+			admin.setVisible(true);
+		}else {
+			//cliente
+			btnComprar.setVisible(true);
+			HomeCliente pantCliente = new HomeCliente();
+			pantCliente.setVisible(true);
 
+		}
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		lblNewLabel.setBounds(54, 36, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		
+	}
 }
